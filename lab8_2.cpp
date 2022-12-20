bfd6675ea964e44e50e6d8391325bdc434027661
MeThussss <mathatt16@gmail.com>
2022-12-20 15:54:27 +0700

    Lab8-2 Complete

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -1,18 +1,42 @@
-//Dialog of the first example is given below.
+#include <iostream>
+#include <string>
+using namespace std;
 
-/*
-Fahsai: Sawadee ka...Can you tell me your name?
-?????: Luke Skywalker
-Fahsai: Wow!!! Luke Skywalker is a really cool name.
-Fahsai: I think you are an Engineering student. What is your student ID?
-Luke Skywalker: 590610999
-Fahsai: I think you may be GEAR 47. I have a free movie ticket for you.
-Fahsai: Let's go to the cinema together!!!
-Fahsai: What movie do you want to watch?
-Luke Skywalker: Star Wars VII
-Fahsai: So....which day are you free to go with me?
-Luke Skywalker: Next Monday
-Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars VII with you.
-Luke Skywalker: May the Force be with you krub
-Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
-*/
+int main(){
+    string a;
+    string b;
+    string c;
+    string d;
+    string x;
+    
+    cout <<"Fahsai: Sawadee ka...Can you tell me your name?" << "\n";
+    cout <<"?????: ";
+    getline(cin,a);
+    
+    cout <<"Fahsai: Wow!!! "<< a <<" is a really cool name."<<"\n";
+    cout <<"Fahsai: I think you are an Engineering student. What is your student ID?"<< "\n";
+    cout <<a <<": ";
+    cin >>x;
+    cin.ignore();
+    
+    cout <<"Fahsai: I think you may be GEAR " << (((int(x[0]) - '0')*10) + (int(x[1]) - '0')) - 12 << "." <<" I have a free movie ticket for you." << "\n";
+    cout <<"Fahsai: Let's go to the cinema together!!!" << "\n";
+    cout <<"Fahsai: What movie do you want to watch?" << "\n";
+    cout << a <<": ";
+    getline(cin,b);
+    
+    cout <<"Fahsai: So....which day are you free to go with me?" << "\n";
+    cout << a <<": ";
+    getline(cin,c);
+    
+    cout <<"Fahsai: "<< c <<"....that is OK!!! I'm looking forward to watching " << b <<" with you."<<"\n";
+    cout << a <<": ";
+    getline(cin,d);
+    cout <<"Fahsai: 555+ see you "<< c <<". Bye Bye \\(^ ^)/"<<"\n";
+    
+    return 0;
+    
+    
+    
+
+}
